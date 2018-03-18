cb4efff447c4148dd35255754c5ba2749063464e
Heefoo <heefoo@outlook.com>
2018-03-18 18:51:14 +0200

    :sparkles: Add rum

--- project.clj
+++ project.clj
@@ -4,13 +4,15 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  
-  
+
+
   :min-lein-version "2.7.1"
 
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/clojurescript "1.9.946"]
-                 [org.clojure/core.async  "0.4.474"]]
+
+ [rum "0.11.2"]
+                 [org.clojure/core.async  "0.4.474" ]]
 
   :plugins [[lein-figwheel "0.5.15"]
             [lein-cljsbuild "1.1.7" :exclusions [[org.clojure/clojure]]]]
--- src/testing_clojurescript/core.cljs
+++ src/testing_clojurescript/core.cljs
@@ -1,11 +1,28 @@
 (ns testing_clojurescript.core
-    (:require ))
+    (:require
+     [rum.core :as rum]
+     [goog.dom :as gdom]
+     )
+
+    )
 
 (enable-console-print!)
 
+(defn find-child-node [parent-node query-string]
+  "Find the first child element that satisfies a css selector"
+  (try
+    (first (array-seq  (.querySelectorAll parent-node query-string)))
+    (catch js/Object e (str "Error :" e))))
+
+
 (println "This text is printed from src/testing_clojurescript/core.cljs. Go ahead and edit it and see reloading in action.")
 
-;; define your app data so that it doesn't get over-written on reload
+
+(rum/defc label [n text]
+  [:div {:style {:color "blue" :font-size "30px" }}
+   (  for [ i (range n)]
+     [:div {:key (str "niklo perneis ton poulo " i " fores " )} text ]) ])
+
 
 (defonce app-state (atom {:text "Hello world!"}))
 
@@ -15,3 +32,7 @@
   ;; your application
   ;; (swap! app-state update-in [:__figwheel_counter] inc)
 )
+
+
+
+(rum/mount (label 5 "abc") (gdom/getElement "app") )
